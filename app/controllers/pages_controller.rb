d11b4f817f70f108f4ee44db8a26478f936071ad
Dexter Rivas <drivas@cs.utsa.edu>
2011-11-20 16:44:35 -0600

    added titles to pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = 'Home'
   end
 
   def contact
+    @title = 'Contact'
   end
 
   def signin
+    @title = 'Sign in'
   end
 
 end
