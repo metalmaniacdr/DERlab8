de3dc71ed41627f64c73f56ddd3be28668fba92f
Dexter Rivas <drivas@cs.utsa.edu>
2011-11-27 14:01:32 -0600

    fixed titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
   def home
     @title = 'Home'
+    @message = 'W e l c o m e !'
   end
 
   def contact
     @title = 'Contact'
+    @message = 'Contact us'
   end
 
   def signin
     @title = 'Sign in'
+    @message = 'Sing in or sign up'
   end
 
 end
